8223a5db340aabbe53eaf5a12b293c21ab2984e4
FLORIAN GUSTIN <floriangustin@florians-macbook-pro-1.home>
2023-02-18 21:27:11 +0100

    fix

--- src/main/java/org/example/domain/Task.java
+++ src/main/java/org/example/domain/Task.java
@@ -4,9 +4,7 @@ import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 
-enum TaskState{
-    TODO, PENDING, PROGRESS, DONE, CANCELLED, CLOSED
-}
+
 
 public class Task {
 
---
+++ src/main/java/org/example/domain/TaskState.java
@@ -0,0 +1,5 @@
+package org.example.domain;
+
+public enum TaskState{
+    TODO, PENDING, PROGRESS, DONE, CANCELLED, CLOSED
+}
